181cc1a429b92b9d321ed57deeddc7501d9aedc2
GeeTransit <geetransit@gmail.com>
2018-11-17 14:05:46 -0500

    Primitive Paint Alpha 1.2 Released
    Current stable is now Alpha 1.2

--- Primitive Paint Alpha 1.2.py
+++ Primitive Paint Alpha 1.2.py
@@ -1,7 +1,7 @@
 ##########################################################################
 ##Program Author: George Zhang                                          ##
 ##Revision Date: Nov 12, 2018                                           ##
-##Program Name: Graphical Canvas                                        ##
+##Program Name: Primirive Paint Alpha 1.2                               ##
 ##Description: This program draws on the canvas of a tkinter window.    ##
 ##########################################################################
 
